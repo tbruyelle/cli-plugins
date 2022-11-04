48f391b4e228f8cc243a0f3ca48902eb7d4837a9
Developer Experience team at Tendermint <hello@tendermint.com>
2022-11-04 14:58:03 +0100

    Initialized with Ignite CLI

--- test1/main.go
+++ test1/main.go
@@ -19,26 +19,32 @@ type p struct{}
 
 func (p) Commands() ([]plugin.Command, error) {
 	// TODO: write your command list here
-	cmds := []plugin.Command{
-		{
-			Use:               "test1",
-			Short:             "Explain what the command is doing...",
-			Long:              "Long description goes here...",
-			PlaceCommandUnder: "ignite",
-			// Examples of subcommands:
-			Commands: []plugin.Command{
-				{Use: "add"},
-				{Use: "list"},
-				{Use: "delete"},
-			},
+	// Here the default is a single test1 command added to the root ignite
+	// command.
+	cmd := plugin.Command{
+		Use:               "test1",
+		Short:             "Explain what the command is doing...",
+		Long:              "Long description goes here...",
+		PlaceCommandUnder: "ignite",
+		// Examples of adding subcommands:
+		Commands: []plugin.Command{
+			{Use: "add"},
+			{Use: "list"},
+			{Use: "delete"},
 		},
 	}
-	return cmds, nil
+	// Example of adding flags
+	cmd.Flags().String("my-flag", "", "a flag example")
+
+	return []plugin.Command{cmd}, nil
 }
 
 func (p) Execute(cmd plugin.Command, args []string) error {
 	// TODO: write command execution here
-	fmt.Printf("Hello I'm the test1 plugin!\nargs=%v, with=%v\n", args, cmd.With)
+	fmt.Printf("Hello I'm the test1 plugin\n")
+	fmt.Printf("My args: %v\n", args)
+	fmt.Println("My flags: my-flag=%s\n", cmd.Flags().GetString("my-flag"))
+	fmt.Println("My config parameters: %v\n", cmd.With)
 
 	// This is how the plugin can access the chain:
 	c, err := getChain(cmd)
